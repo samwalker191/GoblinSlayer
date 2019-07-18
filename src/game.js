3b93897318542c26c831072d5e52b4633f8c7567
Sam Walker <sewalker191@gmail.com>
2019-07-17 17:02:51 -0700

    Add #drawBoard to Game

--- src/game.js
+++ src/game.js
@@ -1,10 +1,14 @@
-
+const Constants = require('./util/constants');
 class Game {
     constructor() {
 
     }
 
-    draw(ctx) {
-        ctx.clearRect(0, 0, )
+    drawBoard(ctx) {
+        ctx.clearRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
+        ctx.fillRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
     }
 }
+
+module.exports = Game
