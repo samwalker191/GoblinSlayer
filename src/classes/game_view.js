6550452d855c3ce70824838e7fb292968b46d387
Sam Walker <sewalker191@gmail.com>
2019-12-10 00:01:01 -0800

    Add new game on keypress

--- src/classes/game_view.js
+++ src/classes/game_view.js
@@ -1,9 +1,12 @@
-const LevelOne = require("../util/levels/level1");
 const Game = require("./game");
+const LevelOne = require("../util/levels/level1");
+const whichTransitionEvent = require('../util/transition_detect_util');
+
 
 class GameView {
     constructor(game) {
         this.game = game;
+        this.pause = false;
     }
 
     start() {
@@ -16,19 +19,46 @@ class GameView {
 
     restart() {
         let gameOver = document.getElementsByClassName('game-over')[0];
-        gameOver.classList.remove('animate-fade');
+        let cont = document.getElementsByClassName('cont')[0];
+        let kills = document.getElementById('kills');
+        let transitionEvent;
         
-        this.game.bindKeyListeners();
-        this.game.reset();
+        kills.innerHTML = `Goblins Slewn: ${this.game.kills}`;
+        const transitionFunc = () => {
+            this.newGame();
+            this.game.bindKeyListeners();
+            this.pause = false;
+            document.removeEventListener('keydown', transitionFunc);
+        }
+        const ded = () => {
+
+            gameOver.classList.remove('animate-fade');
+            kills.classList.remove('animate-fade');
+            transitionEvent = whichTransitionEvent();
+
+            if (transitionEvent) {
+                gameOver.addEventListener(transitionEvent, () => {
+                    setTimeout(() => {
+                        cont.classList.remove('animate-fade');
+                        document.addEventListener('keydown', transitionFunc)
+                    }, 400);
+                });
+            } 
+        }
+        ded();
     }
 
-    gameOver() {
+    newGame() {
         let boardCanvas = document.getElementById('board-canvas');
         let animateCanvas = document.getElementById('animate-canvas');
         let attackCanvas = document.getElementById('attack-canvas');
         let level1 = new LevelOne();
         let levels = [level1];
         this.game = new Game(boardCanvas, animateCanvas, attackCanvas, levels);
+    }
+
+    gameOver() {
+        this.pause = true;
         this.restart();
     }
 
@@ -38,11 +68,15 @@ class GameView {
         if (this.game.checkGameOver()) {
             this.gameOver();
         }
-        this.game.step(timeDelta);
+        if (!this.pause) {
+            this.game.step(timeDelta);
+        }
         this.lastTime = time;
     }
 
     showGame() {
+        let gameOver = document.getElementsByClassName('game-over')[0];
+        gameOver.classList.remove('hidden');
         const gameContainer = document.getElementsByClassName('game-container')[0];
         gameContainer.classList.remove('game-container');
         gameContainer.classList.add('game-container-active');
